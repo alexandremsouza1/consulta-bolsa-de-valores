ac93f0badbd5a2643af0a2954d1c4cbeffe5913c
franklindrw <franklindrw@gmail.com>
2022-11-27 23:15:48 -0300

    enviando dados para tabela

--- src/buscaTitulos.js
+++ src/buscaTitulos.js
@@ -1,11 +1,12 @@
 const form = document.getElementById("buscaTituloForm");
 const pluList = document.getElementById("titulos");
+const tbody = document.getElementById("tableBody");
 
 form.addEventListener("submit", async function (e) {
     e.preventDefault();
     const array = _stringParaArray(pluList.value);
     data = await consultaTitulos(array);
-    montaTabela(data, array);
+    montaTabela(data, array, tbody);
 });
 
 function _stringParaArray(string) {
--- src/view.js
+++ src/view.js
@@ -1,7 +1,24 @@
-function montaTabela(data, moedas) {
+function montaTabela(data, moedas, tabela) {
+    let html = [];
     data.forEach((moeda, index) => {
         let title = moedas[index] + "BRL";
         let moedaData = moeda[title];
-        console.log(moedaData);
+        html.push(tabelaHtml(moedaData));
     });
+    tabela.innerHTML = html;
+}
+
+function tabelaHtml(moedaData) {
+    let linha = `
+    <tr>
+        <td>${moedaData.code}</td>
+        <td>${moedaData.name}</td>
+        <td>${moedaData.ask}</td>
+        <td>${moedaData.bid}</td>
+        <td>${moedaData.pctChange}%</td>
+        <td>${moedaData.create_date}</td>
+    </tr>
+    `;
+
+    return linha;
 }
